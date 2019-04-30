5411fc94ee50f68937f9ee814fab98973c27283a
ashutosh <ashutoshb@gmail.com>
2019-04-30 12:23:38 +0200

    fix import for python 3 compatibility

--- knmi/__init__.py
+++ knmi/__init__.py
@@ -1,2 +1,2 @@
-from knmi import get_day_data_raw, get_day_data_dataframe
-from metadata import stations, variables
+from .knmi import get_day_data_raw, get_day_data_dataframe
+from .metadata import stations, variables
